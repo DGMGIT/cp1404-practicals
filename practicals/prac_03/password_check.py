245bc8399061df1a153fef5232a6991b5a7d56c7
Tino Tusaumwe <tinotenda.tusaumwe@my.jcu.edu.au>
2020-08-27 14:48:27 +1000

    Refactor password_check.py to use functions

--- practicals/prac_03/password_check.py
+++ practicals/prac_03/password_check.py
@@ -1,2 +1,15 @@
-password = input("Enter a password: ")
-print("*" * len(password))
+def main():
+    password = get_password()
+    print(hide_password(password))
+
+
+def get_password():
+    return input("Enter a password: ")
+
+
+def hide_password(password):
+    return "*" * len(password)
+
+
+if __name__ == "__main__":
+    main()
