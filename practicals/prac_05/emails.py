257ba1f769454073011228c8cc838c02b336c290
Tino Tusaumwe <tinotenda.tusaumwe@my.jcu.edu.au>
2020-09-05 10:15:03 +1000

    Add ability to add name
    Rename get_name to get_name_from
    Add ability to add a new name if the name couldn't be retrieved from
    the email.

--- practicals/prac_05/emails.py
+++ practicals/prac_05/emails.py
@@ -3,20 +3,23 @@ def main():
 
     email = input("Enter your email: ").strip()
     while len(email) != 0:
-        name = get_name(email)
-        answer = get_response("Is your name {} y/n ".format(name))
+        name = get_name_from(email)
+        answer = get_response("Is your name {}? y/n ".format(name))
         if answer == 'y':
             if name in name_to_email and name_to_email[name] == email:
                 print("That name and email already exists")
             else:
                 name_to_email[name] = email
+        else:
+            name = input("Name: ")
+            name_to_email[name] = email
         email = input("Enter your email: ").strip().lower()
 
     for key in name_to_email:
         print("{} ({})".format(key, name_to_email[key]))
 
 
-def get_name(email):
+def get_name_from(email):
     first_chunk = email.split("@")
     if len(first_chunk) == 0:
         print("Invalid email")
@@ -32,7 +35,7 @@ def get_response(prompt):
     answer = ""
     while should_try_again:
         should_try_again = False
-        answer = input(prompt).strip().lower()
+        answer = input(prompt).strip().lower()[0]
         if len(answer) == 0 or answer == 'y':
             answer = 'y'
         elif answer == 'n':
